abc701b06917ad2ff775b7e120afc6414c708f9d
akhabibullina <akhabibullina@aisquared.com>
2015-02-18 16:37:28 +0200

    Add basic validation and show result of the operation.

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
 $(document).ready(function() {
-  document.querySelector('form').addEventListener('submit', function() {
+  document.querySelector('form').addEventListener('submit', function(e) {
 
-    var length = $(this).find('input[name="size"]')[0].value;
-    var boardString = $(this).find('input[name="board"]')[0].value;
+    // Cancel form events
+    e.preventDefault();
+    e.stopPropagation();
 
-    // todo: validation
+    // Get form values
+    var length = +($(this).find('input[name="size"]')[0].value); // convert to num
+    var boardString = '' + ($(this).find('input[name="board"]')[0].value); // convert to string
 
-    // todo: fix something doesn't work right
-    alert(findWinner(length, boardString));
+    // Validate
+    var isValidLength = validateLength(length);
+    var isValidBoardString = validateBoardString(boardString, length);
+
+    // Calculate result
+    if (isValidLength && isValidBoardString) {
+      var winner = findWinner(length, boardString);
+      showResult(winner || 'undefined');
+    } else {
+      showValidationErrorMessage();
+    }
 
   });
+
+  function validateLength(length) {
+    if (length) {
+      return length >= 1 && length <= 100;
+    }
+    return false;
+  }
+
+  function validateBoardString(boardString, length) {
+    if (boardString) {
+      return boardString.length === Math.pow(length, 2);
+    }
+  }
+
+  function showResult(winner) {
+    document.querySelector('#result').innerHTML = winner;
+  }
+
+  function showValidationErrorMessage() {
+    alert('Validation Error!');
+  }
 });
