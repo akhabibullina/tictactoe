c18fea0576e51cb84c4668205d93e789e0b93114
akhabibullina <ascii88@mail.ru>
2015-03-08 01:36:14 +0200

    #13 If board length is greater than 10 then do not visualize the board(it's too large).

--- javascripts/visualizer.js
+++ javascripts/visualizer.js
@@ -1,26 +1,31 @@
 'use strict';
 
 function visualizeBoard(length, boardString) {
+  var newBoard;
+  if (length <= 10) {
+    var breakExpr = new RegExp('.{1,' + length + '}', 'g');
 
-  var breakExpr = new RegExp('.{1,' + length + '}', 'g');
+    var boardArray1d = boardString.match(breakExpr);
+    var boardArray2d = populateArray(length, boardArray1d);
 
-  var boardArray1d = boardString.match(breakExpr);
-  var boardArray2d = populateArray(length, boardArray1d);
+    // Draw table
+    var table$ = $('<table/>').attr('id', 'ttt-visualizer-table');
+    for (var rowIndex = 0; rowIndex < length; rowIndex++) {
+      // Rows
+      var row$ = $('<tr/>');
+      for (var colIndex = 0; colIndex < length; colIndex++) {
+        // Cols
+        var col$ = $('<td/>');
+        var cellValue = boardArray2d[rowIndex][colIndex];
 
-  // Draw table
-  var table$ = $('<table/>').attr('id', 'ttt-visualizer-table');
-  for (var rowIndex = 0; rowIndex < length; rowIndex++) {
-    // Rows
-    var row$ = $('<tr/>');
-    for (var colIndex = 0; colIndex < length; colIndex++) {
-      // Cols
-      var col$ = $('<td/>');
-      var cellValue = boardArray2d[rowIndex][colIndex];
-
-      col$.attr('class', (rowIndex + 1) + '_' + (colIndex + 1)).html(cellValue);
-      row$.append(col$);
+        col$.attr('class', (rowIndex + 1) + '_' + (colIndex + 1)).html(cellValue);
+        row$.append(col$);
+      }
+      table$.append(row$);
+      newBoard = table$;
     }
-    table$.append(row$);
+  } else {
+    newBoard = '<img style="height:100%" src="images/Super Mario - Question Mark Block.jpg" />';
   }
-  $("#ttt-visualizer-box").html(table$);
+  $("#ttt-visualizer-box").html(newBoard);
 }
